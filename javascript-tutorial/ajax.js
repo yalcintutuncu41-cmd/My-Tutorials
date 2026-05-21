29a086549c90825db26fe377ac3c81466b9e16f6
yalcintutuncu <yalcintutuncu41@gmail.com>
2026-05-21 10:09:10 +0300

    .

--- javascript-tutorial/ajax.js
+++ javascript-tutorial/ajax.js
@@ -1,2 +1,11 @@
-const xhr = new XMLHttpRequest();
-console.log(xhr);
+function getData(url){
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", ()=>{
+        if(xhr.readyState===4 && xhr.status ===200){
+            document.write(JSON.parse(xhr.response))
+        }
+    })
+    xhr.open("GET", url);
+    xhr.send();
+}
+getData("https://jsonplaceholder.typicode.com/posts");
